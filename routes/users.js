4a139eff15f49b2815ec99c6a76a228076d45dcf
Jen000 <>
2021-12-27 17:07:43 -0500

    updating routes

--- routes/users.js
+++ routes/users.js
@@ -23,35 +23,6 @@ router.delete('/:id', deleteUser);
 router.patch('/:id', patchUser);
 
 
-//router.get('/key', getAuthentication);
-
-
-
-=======
-import express from 'express';
-
-import {getUsers, createUser, getUser, deleteUser, patchUser} from '../controllers/usersControllers.js';
-
-
-const router = express.Router();    //initalize router
-
-// all routes in here are starting with /users
-router.get('/', getUsers);
-
-
-// overrides old info or creates new info
-router.post('/', createUser);
-
-
-router.get('/:id', getUser);
-
-
-router.delete('/:id', deleteUser);
-
-
-// patch updates a user
-router.patch('/:id', patchUser);
-
 
 //router.get('/key', getAuthentication);
 
