58a96beee4ea05f144ec4d19d00c2dc9c5067ad8
Jen000 <>
2021-12-28 16:34:29 -0500

    CONNECTING THE DATABASE

---
+++ routes/database.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  });
+
+export default router;
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 // connect to the database
 
 // https://devcenter.heroku.com/articles/getting-started-with-nodejs#provision-a-database
-/*const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-*/
+import pg from 'pg';
+import dotenv from 'dotenv';
+const {Pool} = pg;
+dotenv.config();
+const databaseConfig = {connectionString: process.env.DATABASE_URL};
+const pool = new Pool(databaseConfig);
+
+export default pool;
+
 
 // configure the application and create routes
 
@@ -18,17 +19,20 @@ import express from 'express';   // call express or us import express from 'expr
 const app = express();             // define app using express
 import bodyParser from 'body-parser';
 import usersRoutes from './routes/users.js';
+import dbRoutes from './routes/database.js';
 
 
 const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());  // will be using json data in the app
 
 app.use('/users', usersRoutes);
+app.use('/db', dbRoutes);
 
 // Routes for API
 // ======================================================================
 
 app.get('/', (req, res) => res.send('Hello from Homepage.'));
+
 
 // START THE SERVER
 // ======================================================================
