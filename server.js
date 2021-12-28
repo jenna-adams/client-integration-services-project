7aa92790273e3a02f2a051decb6c3b597bd86ae5
Jen000 <>
2021-12-28 13:32:47 -0500

    removed engines, ensured dynamic port

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+// connect to the database
+
+// https://devcenter.heroku.com/articles/getting-started-with-nodejs#provision-a-database
+/*const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+*/
+
 // configure the application and create routes
 
 // call the packages needed
@@ -8,7 +20,7 @@ import bodyParser from 'body-parser';
 import usersRoutes from './routes/users.js';
 
 
-const PORT1 = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());  // will be using json data in the app
 
 app.use('/users', usersRoutes);
@@ -21,4 +33,5 @@ app.get('/', (req, res) => res.send('Hello from Homepage.'));
 // START THE SERVER
 // ======================================================================
 
-app.listen(PORT1, () => console.log(`Server Running on port: http://localhost:${PORT1}`));
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
+
