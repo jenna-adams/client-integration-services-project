bf925b3b134f2b1a676a7d4fcca12711101775a9
Jen000 <>
2021-12-30 10:18:34 -0500

    testing database on heroku

--- db/database.js
+++ db/database.js
@@ -3,26 +3,11 @@ import dotenv from 'dotenv';
 const {Pool} = pg;
 dotenv.config();
 const pool = new Pool({
-  user: process.env.USER || "postgres",
-  host: process.env.HOST || "localhost",
-  database: process.env.DATABASE || "postgres",
-  password: process.env.PASSWORD || "jenna",
-  port: process.env.PORT || 5432
+  user: process.env.USER,// || "postgres",
+  host: process.env.HOST,// || "localhost",
+  database: process.env.DATABASE,// || "postgres",
+  password: process.env.PASSWORD,// || "jenna",
+  port: process.env.PORT,// || 5432
 });
 
 export default pool;
-
-/*
-router.get('/', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-});
-*/
