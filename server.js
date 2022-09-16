78f7fbe3a392ce6562c83f74a79014e2d3c408b4
Jenna Adams <jenna@Jennas-Air.localdomain>
2022-09-16 18:32:34 -0400

    editing auth

--- server.js
+++ server.js
@@ -15,6 +15,19 @@ app.use(bodyParser.json());  // will be using json data in the app
 
 // middleware
 app.use(express.json());
+const router = express.Router();
+
+router.use((req, res, next) => {
+    const headers = req.headers
+    const clientToken = headers["authorization"] || headers["x-access-token"];
+    const serverToken = process.env.TOKEN;
+    const verified = clientToken === serverToken;
+
+    if (!verified) {
+        return res.status(400).send("Invalid token.");
+    }
+    next();
+});
 
 app.use('/api', routes);
 
