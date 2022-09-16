62ac1a15aa41533c48da6e57b179f1177e0f2000
Jenna Adams <jenna@Jennas-Air.localdomain>
2022-09-16 14:52:09 -0400

    i wonder

--- routes/queries.js
+++ routes/queries.js
@@ -1,6 +1,6 @@
 const getUsers = "SELECT * FROM test_table";
-const getUser = "SELECT * FROM test_table WHERE username = $1";
-const getUserWithToken = "SELECT * FROM test_table WHERE api_token = $1";
+const getUser = "SELECT * FROM test_table WHERE username = $6";
+const getUserWithToken = "SELECT * FROM test_table WHERE api_token = $2";
 const checkUserId = "SELECT s FROM test_table s WHERE s.id = $1";
 const addUserToDb = "INSERT INTO test_table (id, firstName, lastName, age) VALUES ($1, $2, $3, $4)";
 const removeUser = "DELETE FROM test_table WHERE id = $1";
