a8fbe78bbf76aeccdc5038da55f8915fb51b7615
Jenna Adams <jenna@Jennas-Air.localdomain>
2022-09-16 18:30:42 -0400

    editing auth

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -4,41 +4,35 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const getAuthToken = async (req, res) => {
-
-    if(req.get('Authorization') != process.env.ELEOS_PLATFORM_KEY) {
-        res.send(req.get('Authorization'));
-    }
-    else {
-        try{
-            const token = req.params.token;
-            // var decodedToken = jwtDecode(token);
-            // var userToken = Object.values(decodedToken);
-            var userInfo;
-            client.query(queries.getUserWithToken, [token], (error, results) => {
-                if (error) {
-                    res.send("There is an error/ get user auth");
-                    throw error;
-                }
-                res.status(200).json(results.rows);
-            })
-
-            var encodedToken = jwt.encode({fullname : user[fullName], username : userInfo[username]}, "code", 'HS256')
-
-            const response = {
-                api_token : encodedToken,
-                full_name : userInfo[fullName],
-                menu_code : userInfo[menu],
-                dashboard_code : userInfo[dashboard],
-                custom_settings_form_code : userInfo[settingForm],
-                theme : userInfo[theme],
-                username : user[username] 
+    try{
+        const token = req.params.token;
+        // var decodedToken = jwtDecode(token);
+        // var userToken = Object.values(decodedToken);
+        var userInfo;
+        client.query(queries.getUserWithToken, [token], (error, results) => {
+            if (error) {
+                res.send("There is an error/ get user auth");
+                throw error;
             }
-            // res.status(200).send(userInfo);
+            res.status(200).json(results.rows);
+        })
 
-        }catch(error){
-            res.send(error);
+        var encodedToken = jwt.encode({fullname : user[fullName], username : userInfo[username]}, "code", 'HS256')
+
+        const response = {
+            api_token : encodedToken,
+            full_name : userInfo[fullName],
+            menu_code : userInfo[menu],
+            dashboard_code : userInfo[dashboard],
+            custom_settings_form_code : userInfo[settingForm],
+            theme : userInfo[theme],
+            username : user[username] 
         }
-     }
+        // res.status(200).send(userInfo);
+
+    }catch(error){
+        res.send(error);
+    }
 
 };
 
