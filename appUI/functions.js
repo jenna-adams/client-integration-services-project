fcf43141ea9a69a55d36c9b8dbd8e5276b939c87
Jenna Adams <jenna@Jennas-MacBook-Air.local>
2022-10-21 21:57:08 -0400

    updated functions.js

--- appUI/functions.js
+++ appUI/functions.js
@@ -2,17 +2,7 @@ function buttonClicked(){
     location.assign('https://jenna-client-services-integration.onrender.com/api/users');
 }
 
-const thisForm = document.getElementById('userForm');
-thisForm.addEventListener('click', async function (e) {
-    e.preventDefault();
-    const formData = new FormData(thisForm).entries()
-    console.log(formData)
-    const response = await fetch('https://jenna-client-services-integration.onrender.com/api/users/', {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(Object.fromEntries(formData))
-    });
-
-    const result = await response.json();
-    console.log(result)
-});
+function userButton(){
+    userValue = document.getElementById('token').ariaValueMax;
+    console.log(userValue)
+}
