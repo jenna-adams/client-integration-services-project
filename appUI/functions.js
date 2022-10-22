968965a4ccaf95849c00e1c1b86af8535557c477
Jenna Adams <jenna@Jennas-MacBook-Air.local>
2022-10-21 22:23:25 -0400

    updated functions.js

--- appUI/functions.js
+++ appUI/functions.js
@@ -5,9 +5,11 @@ function buttonClicked(){
 function userButton(){
     const userValue = document.getElementById('token').value;
     console.log(userValue)
+    location.assign('https://jenna-client-services-integration.onrender.com/api/user/' + String(userValue));
 }
 
 function loadButton(){
     const loadValue = document.getElementById('loadID').value;
     console.log(loadValue)
+    location.assign('https://jenna-client-services-integration.onrender.com/api/loads/' + String(loadValue));
 }
